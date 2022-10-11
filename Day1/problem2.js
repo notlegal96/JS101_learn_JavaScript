71be18e619812049336a75c323f629e4a804823f
notlegal96 <shenm09@gmail.com>
2022-10-11 20:27:48 +0530

    Commiting Files

---
+++ Day1/problem2.js
@@ -0,0 +1,9 @@
+let name = "Liju Mathew";
+console.log(name);
+
+ name = "Mathew T.M"; // reassigning value
+console.log(name);
+
+ name = "Laly Mathew"; // reassigning value
+console.log(name);
+
---
+++ Day1/problem4.js
@@ -0,0 +1,36 @@
+let a = "Name: 丂卄🝗𝓝  爪闩七卄🝗山"; 
+
+let a_1 = "School; Morning Star "
+
+let b = "Grade: 🅱"; 
+
+let c = "Section: 𝙁"; 
+
+let d = "Rollnumber: 56";
+
+sub1 = "Ｍａｔｈｓ － ５６／１００";
+sub2 = "Ｅｎｇｌｉｓｈ － ８５／１００";
+sub3 = "Ｐｈｙｓｉｃｓ － ７６／１００";
+
+result = "ᖘᎯ⟆⟆∈ᕍ"
+
+console.log(a);
+console.log();
+console.log(a_1);
+console.log();
+console.log(b);
+console.log();
+console.log(c);
+console.log();
+console.log(d);
+console.log();
+
+console.log(sub1);
+console.log();
+console.log(sub2);
+console.log();
+console.log(sub3);
+console.log();
+
+console.log(result);
+
